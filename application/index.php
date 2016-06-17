2cf25b364b6e0fddb368de58b1bf4b91da7919c2
Emalsha <remalsha@gmail.com>
2016-06-17 08:40:03 +0530

    Folder and file structure
    Arrange file and folders according.

---
+++ application/index.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>e-quiz</title>
+	<link rel="stylesheet" href="../public/css/e-quiz.css">
+</head>
+<body>
+	
+	<div class="header">
+		<center>
+			<a href="#"><img src="../public/img/logo.png" alt="e-qize logo" style="width:20%;padding:10px;"></a>
+		</center>
+	</div>
+
+	<div class="nev">
+		<center>
+			<h1 class="nev_title">WELCOME</h1>
+		</center>
+	</div>
+
+	<div class="container">
+		<div class="container-inside">
+			<h1>Welcome to e-qize</h1>
+			<p>e-qize is a online question generator service.You can create online question paper and publis it to others.</p>
+		</div>
+	</div>
+</body>
+</html>
